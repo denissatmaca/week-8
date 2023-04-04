34d4b51f26ec53564f179fc76c6b68f5da3660f3
deniz atmaca <deniz.atmaca@outlook.com>
2023-04-04 14:04:09 +0300

    week 8

---
+++ Week8/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string[] tvShows = { "Firefly", "Office", "Last of Us", "Friends", "Twin Peaks", "House", "Stranger Things" };
+string[] food = { "sushi", "pizza", "Chinese takeaway", "burger and chips", "chicken wings", "chips", "popcorn" };
+string[] drinks = { "mineral water", "Coca-cola", "apple juice", "milk", "beer" };
+
+string randomMovie = PickRandomFromArray(tvShows);
+string randomFood = PickRandomFromArray(food);
+string randomDrink = PickRandomFromArray(drinks);
+
+Console.WriteLine($"I recommend you to watch {randomMovie} tonight ");
+Console.WriteLine($"Order some {randomFood} and {randomDrink}.");
+
+//PickRandomFromArray(tvShows);
+//PickRandomFromArray(food);
+//PickRandomFromArray(drinks);
+
+//Random rnd = new Random();
+
+//int randomIndex = rnd.Next(0, tvShows.Length);
+
+
+//string randomMovie = tvShows[randomIndex];
+
+//Console.WriteLine(randomMovie);
+
+//randomIndex = rnd.Next(0,food.Length);
+//string randomFood = food[randomIndex];
+//Console.WriteLine(randomFood);
+
+//randomIndex = rnd.Next(0, drinks.Length);   
+//string randomDrink = drinks[randomIndex];
+//Console.WriteLine(randomDrink);
+
+static string  PickRandomFromArray(string[] someArray){
+    Random rnd= new Random();
+    
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomElement = someArray[randomIndex];
+    return randomElement;
+    
+}
